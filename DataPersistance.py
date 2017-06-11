534fb0c65b467a2c8ccdf8b3d2b88b9c5593e913
garfield <18174083007@163.com>
2017-06-11 09:19:42 +0800

    2017-6-11

--- DataPersistance.py
+++ DataPersistance.py
@@ -21,17 +21,27 @@ class DataPersistance():
         cursor = self.obtain_connect().cursor()
         return cursor
 
-    def add_data(self,partnumber,datasheet1,datasheet2):
-        db = self.obtain_connect()
-        cursor = db.cursor()
-        SQL_ADD = 'INSERT INTO t_result(partnumber,datasheet1,datasheet2) VALUES ("%s","%s","%s")' %(partnumber, datasheet1,datasheet2)
-        try:
-            print(SQL_ADD)
-            cursor.execute(SQL_ADD)
-            print('数据写入成功：%s, %s, %s' % (partnumber, datasheet1, datasheet2))
-            db.commit()
-        except Exception as e:
-            db.rollback()
-            print(e)
-        db.close()
+    def add_data(self, partnumber, dic):
+        for k, v in dic.items():
+            product_type = k
+            url = v
+
+            db = self.obtain_connect()
+            cursor = db.cursor()
+            SQL_ADD = 'INSERT INTO result(pn,url , product_type) VALUES ("%s","%s","%s")' % (partnumber, url, product_type)
+            SQL_SEARCH = 'SELECT pn, url, product_type FROM result WHERE pn = "%s" and url = "%s" and product_type ="%s"' % (partnumber, url, product_type)
+            try:
+                cursor.execute(SQL_SEARCH)
+                result = cursor.fetchall()
+                if len(result) == 0:
+                    cursor.execute(SQL_ADD)
+                    print(SQL_ADD)
+                    db.commit()
+                    print('数据写入成功：%s, %s, %s' % (partnumber, url, product_type))
+                else:
+                    print("数据已存在")
+            except Exception as e:
+                db.rollback()
+                print(e)
+            db.close()
 
--- Downloader.py
+++ Downloader.py
@@ -5,7 +5,7 @@ import threading
 import math
 import DataPersistance
 from lxml import etree
-
+from multiprocessing.dummy import Pool as ThreadPool
 import Config
 
 persistance = DataPersistance.DataPersistance()
@@ -21,12 +21,11 @@ page = 1
 
 def get_page():
     global page
-    total_page = int(math.floor(total_row / per))
+    total_page = int(math.ceil(total_row / per))
     for i in range(total_page):
         page += 1
         data = create_data()
         requests_post(data)
-
 
 
 def create_data():
@@ -41,62 +40,127 @@ def create_data():
 
     return data
 
-# def request_detail():
-#     response = requests.get(url)
-#     print(response.text)
 
-
-def test(url,pn):
+def test(url):
     headers = {
         'Host': 'www.ket.com',
         'Connection': 'keep-alive',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/58.0.3029.110 Safari/537.36',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
         'X-Requested-With': 'XMLHttpRequest',
         'Upgrade-Insecure-Requests': '1',
         'Referer': 'http://www.ket.com/en/product/feature-search.ket?categoryCd=CA01',
         'Accept-Encoding': 'gzip, deflate, sdch',
         'Accept-Language': 'zh-CN,zh;q=0.8',
-        'Cookie': 'JSESSIONID=3502457241A1F141CE25171598C756F7; language=en; org.springframework.web.servlet.i18n.CookieLocaleResolver.LOCALE=en'
+        'Cookie': 'JSESSIONID=3502457241A1F141CE25171598C756F7; language=en; '
+                  'org.springframework.web.servlet.i18n.CookieLocaleResolver.LOCALE=en '
     }
     response = requests.get(url, headers=headers)
     body = etree.HTML(response.text)
-    d2_xpath = body.xpath('//*[@id="container"]/div[3]/div[1]/dl/dd[2]/div[2]/span[1]/a/@href')
-    d3_xpath = body.xpath('//*[@id="container"]/div[3]/div[1]/dl/dd[2]/div[2]/span[2]/a/@href')
-    # if len(d2_xpath) == 0:
-    #     d2_xpath[0] = None
-    # if len(d3_xpath) == 0:
-    #     d3_xpath[0] = None
-    persistance.add_data(pn,d2_xpath,d3_xpath)
+    basic_file_url = 'http://www.ket.com/cm/fileDownMan.ket?attcFilSn='
+    xpath_pdf = body.xpath('//*[@id="container"]/div[3]/div[1]/dl/dd[2]/div[2]/span/a/@href')
+    xpath_file = body.xpath('//*[@id="container"]/div[3]/div[1]/dl/dd[2]/div[2]/span/a/@filesn')
+    product_type = body.xpath('//*[@id="container"]/div[3]/div[1]/dl/dd[2]/div[2]/span/a/text()')
+    partn = url.split('=')[1]
+    pool = ThreadPool(processes=4)
+
+    xpath_file_queue = queue.Queue()
+    for n in range(len(xpath_file)):
+        xpath_file_queue.put(xpath_file[n])
+
+    for i in range(len(product_type)):
+        if xpath_pdf[i] != '#':
+            pdf_url = 'http://www.ket.com/' + str(xpath_pdf[i])
+            dic = {product_type[i]: pdf_url}
+            pool.apply_async(persistance.add_data, (partn, dic))
+        else:
+            file_url = basic_file_url + str(xpath_file_queue.get())
+            dic = {product_type[i]: file_url}
+            pool.apply_async(persistance.add_data, (partn, dic))
+
+    pool.close()
+    pool.join()
 
 
 def requests_post(data):
     response = requests.post(Config.ROOT_URL, data=data, headers=Config.headers_root)
     parser_json(response.text)
 
+
 def request_detail():
     while not Q.empty():
+        # 构造产品详情页的url
         url = Config.DETAIL_URL + Q.get()
-        # test(url)
-        t = threading.Thread(target=test(url,Q.get()))
+        t = threading.Thread(target=test(url,))
         t.start()
     get_page()
 
 
-def parser_html(text):
-    pass
-
 global total_row
+global count
+count = 1
+
+
 def parser_json(response):
     loads = json.loads(response)
+    # 拿到产品总数
     global total_row
     total_row = loads['result']['searchInfo']['totalRow']
-    # print(create_data())
+    # 拿到产品型号
     lists = loads['result']['list']
     for i in range(len(lists)):
         partnumber = lists[i]['PARTNUMBER']
-        Q.put(partnumber)
+
+        if partnumber is None:
+            if count < 3:
+                data = get_category_cd(count)
+                requests_post(data)
+            else:
+                data = get_category(count)
+                requests_post(data)
+        else:
+            global count
+            count = 1
+            Q.put(partnumber)
     request_detail()
+
+# 构造最上一级种类集合
+total_category = set()
+total_category_list = ['A', 'B', 'C', 'd', 'E', 'D', 'H']
+for i in range(len(total_category_list)):
+    total_category.add(total_category_list[i])
+
+
+# 获取最上一级的大种类url编号
+def get_category(count):
+    data = create_data()
+    category = data['categoryCd']
+    category_list = list(category)
+    if total_category is None:
+        return
+    current_category = total_category.pop()
+    category_list[1] = current_category
+    count += 1
+    category = "".join(category_list)
+    data['categoryCd'] = category
+    return data
+
+
+# 获取大分类下小种类url编号
+def get_category_cd(count):
+    data = create_data()
+    categorycd = data['categoryCd']
+    category_cd_list = list(categorycd)
+    n = int(category_cd_list[3])
+    n += 1
+    category_cd_list[3] = str(n)
+    count += 1
+    category = "".join(category_cd_list)
+    data['categoryCd'] = category
+    print(data)
+    return data
+
 
 if __name__ == '__main__':
     response = requests.post(Config.ROOT_URL, data=create_data(), headers=Config.headers_root)
